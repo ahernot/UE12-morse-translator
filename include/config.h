bde3910bfa4e2d34e0042c16efadece7ec312ae3
ahernot <70817470+ahernot@users.noreply.github.com>
2020-12-20 00:15:04 +0100

    minor update

--- include/config.h
+++ include/config.h
@@ -7,3 +7,4 @@
 #define XCHAR_BREAK 1 // gap between impulses
 #define CHAR_BREAK 3 // gap between characters
 #define WORD_BREAK 7 // gap between words (space)
+#define SENTENCE_BREAK 9 // full-stop
