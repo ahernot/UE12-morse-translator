ca32ac1d26ed91b7a4895e75abb9882944ff74cc
ahernot <70817470+ahernot@users.noreply.github.com>
2020-12-19 20:44:23 +0100

    Create config.h

---
+++ include/config.h
@@ -0,0 +1,9 @@
+
+#define TIME_UNIT 10 // 10ms
+
+#define HIGH 3 // length of dash
+#define LOW 1 // length of dot
+
+#define XCHAR_BREAK 1 // gap between impulses
+#define CHAR_BREAK 3 // gap between characters
+#define WORD_BREAK 7 // gap between words (space)
