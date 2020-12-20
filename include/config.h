aa6a3aa5941af3c2fb754919ae16c03336ef4423
ahernot <70817470+ahernot@users.noreply.github.com>
2020-12-20 11:02:45 +0100

    Update config.h

--- include/config.h
+++ include/config.h
@@ -1,10 +1,13 @@
 
 #define TIME_UNIT 10 // 10ms
 
-#define HIGH 3 // length of dash
-#define LOW 1 // length of dot
+#define HIGH_TIME 3 // length of dash
+#define LOW_TIME 1 // length of dot
 
-#define XCHAR_BREAK 1 // gap between impulses
-#define CHAR_BREAK 3 // gap between characters
-#define WORD_BREAK 7 // gap between words (space)
-#define SENTENCE_BREAK 9 // full-stop
+#define HIGH_REPR 1 // yes signal
+#define LOW_REPR 0 // no signal
+
+#define XCHAR_BREAK_TIME 1 // gap between impulses
+#define CHAR_BREAK_TIME 3 // gap between characters
+#define WORD_BREAK_TIME 7 // gap between words (space)
+#define SENTENCE_BREAK_TIME 9 // full-stop
