1efef7994b488955330a23a1e83e2375d23cdbcb
ahernot <70817470+ahernot@users.noreply.github.com>
2020-12-20 11:02:41 +0100

    add signal bytestream generation function

--- tests.cpp
+++ tests.cpp
@@ -13,6 +13,20 @@ g++ -std=c++11 tests.cpp -o t
 #include <map>
 #include <queue> // queue to add letters
 
+// TO DEFINE IN config.h
+#define DASH_TIME 3 // length of dash
+#define DOT_TIME 1 // length of dot
+
+#define HIGH_REPR 1 // yes signal
+#define LOW_REPR 0 // no signal
+
+#define XCHAR_BREAK_TIME 1 // gap between impulses
+#define CHAR_BREAK_TIME 3 // gap between characters
+#define WORD_BREAK_TIME 7 // gap between words (space)
+#define SENTENCE_BREAK_TIME 9 // full-stop
+
+
+
 
 
 /**
@@ -113,6 +127,9 @@ std::queue<std::string> convert_from_text (std::string& message,
     // Initialise queue
     std::queue<std::string> conversion_queue;
 
+    const std::string fullstop = ".";
+    const std::string space = "_";
+
     // Loop through the characters in the message string
     for (std::string::iterator it = message.begin(); it != message.end(); it++) {
 
@@ -121,12 +138,16 @@ std::queue<std::string> convert_from_text (std::string& message,
         c = *it;
 
         // Convert characters
-        if (c == " ") { cc = "_"; }
-        else if (c == ".") { cc = "."; }
-        else { std::string cc = convert_char(c, conversion_map, preconversion_map); };
-
-        // Push to queue
-        conversion_queue.push(cc);
+        if (c == " ") {
+            conversion_queue.push(space); // space (word break)
+        }
+        else if (c == ".") {
+            conversion_queue.push(fullstop); // fullstop (sentence break)
+        }
+        else {
+            std::string cc = convert_char(c, conversion_map, preconversion_map);
+            conversion_queue.push(cc);
+        };
         
     };
 
@@ -138,6 +159,126 @@ std::queue<std::string> convert_from_text (std::string& message,
 
 // Function to convert queue to binary signal with 0 and 1, each character lasting for TIME_UNIT milliseconds
 // Function to save output to a WAV file
+
+
+
+std::queue<bool> queueToSignal (std::queue<std::string> conversion_queue) {
+
+    // Initialise signal queue
+    std::queue<bool> signal_queue;
+
+    // Define conversion constants (to move)
+    const std::string dot_repr = "0";
+    const std::string dash_repr = "1";
+    const std::string fullstop = ".";
+    const std::string space = "_";
+
+    // Initialise gap request flag
+    bool queue_gap = false;
+
+    // Initialise eof (end of field) flag
+    bool eof = false;
+
+    while (!conversion_queue.empty()) {
+
+        // Get first item of conversion queue
+        std::string conv_char = conversion_queue.front();
+
+        // Run through element's impulses
+        for (std::string::iterator it = conv_char.begin(); it != conv_char.end(); it++) {
+
+            // Convert from char to string
+            std::string c; std::string cc;
+            c = *it;
+
+            // Dot & Dash
+            if ((c == dot_repr) || (c == dash_repr)) {
+
+                // Add gap between characters (if no space character before/after)
+                if (queue_gap == true) {
+                    //std::cout << ">gap" << std::endl;
+
+                    for (int i=0; i<CHAR_BREAK_TIME; i++) {
+                        signal_queue.push(false);
+                    };
+
+                    // Update flag
+                    queue_gap = false;
+                }
+
+                // Add gap between impulses (if no space character before/after)
+                else { // if not first character
+                    for (int i=0; i<XCHAR_BREAK_TIME; i++) {
+                        signal_queue.push(false);
+                    };
+                };
+
+                // Get impulse length
+                int impulse_time;
+                if (c == dot_repr) {
+                    impulse_time = DOT_TIME;
+                    //std::cout << "dot" << std::endl;
+                }
+                else if (c == dash_repr) {
+                    impulse_time = DASH_TIME;
+                    //std::cout << "dash" << std::endl;
+                };
+
+                // Add impulse
+                for (int i=0; i<impulse_time; i++) {
+                    signal_queue.push(true);
+                };
+
+                // Reset eof (end of field) flag
+                eof = false;
+
+            }
+
+
+
+            // Space & Fullstop
+            else if ( (eof == false) && ((c == space) || (c == fullstop)) ) {
+
+                // Reset gap request flag
+                queue_gap = false;
+
+                // Get gap length
+                int gap_time;
+                if (c == space) {
+                    gap_time = WORD_BREAK_TIME;
+                    //std::cout << ">>>SPACE" << std::endl;
+                }
+                else if (c == fullstop) {
+                    gap_time = SENTENCE_BREAK_TIME;
+                    //std::cout << ">>>FULLSTOP" << std::endl;
+                };
+
+                // Add gap
+                for (int i=0; i<gap_time; i++) {
+                    signal_queue.push(false);
+                };
+
+                // Update eof flag
+                eof = true;
+            };
+
+        };
+
+        // Remove first item from conversion queue
+        conversion_queue.pop();
+
+        // Request gap between characters (will apply if queue not empty)
+        if (eof == false) {
+            queue_gap = true;
+        };
+        
+    };
+
+    
+    return signal_queue;
+
+};
+
 
 
 
@@ -162,6 +303,13 @@ int main () {
     std::string message = "This is a message. Hello.";
     std::queue<std::string> conversion_queue = convert_from_text(message, conversion_map, preconversion_map);
     
+
+    std::queue<bool> signal_queue = queueToSignal(conversion_queue);
+    while (!signal_queue.empty()) {
+        bool cbool = signal_queue.front();
+        //std::cout << cbool << std::endl;
+        signal_queue.pop();
+    };
 
     return 0;
 };
