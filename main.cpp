52e6bb111136e002291ff3990f2b49675044a204
ahernot <70817470+ahernot@users.noreply.github.com>
2020-12-23 21:17:21 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 // #include "stdlib.h"
 
 #include "config.h"
+
+#include "morse.h"
+#include "signal.h"
 #include "pcm.h"
 #include "wav.h"
 
@@ -11,15 +14,40 @@
 
 
 
-void textToMorseAudio (const std::string text,
-                       const std::string outFilePath) {
 
+void textToMorseAudio (std::string& text, // cannot be const because of iterator
+                       const std::string outFilePath,
+                       std::map<std::string, std::string> conversionMap,
+                       std::map<std::string, std::string> preconversionMap) {
+    
+
+    // Convert from text to morse sequence
+    std::queue<std::string> conversionQueue = stringToMorse(text, conversionMap, preconversionMap);
+    
+    // Convert from morse sequence to signal bytestream queue
+    std::queue<bool> signalQueue;
+    queueToSignal(conversionQueue, signalQueue); // check that conversion_queue remains unmodified (ideally would be popped and deleted)
+
+    // Generate PCM signal to include into WAV file
+    std::vector<int> signalPCM = generateSignalPCM (signalQueue, 44100, 1);
+    
 
     };
 
 
 
 int main () {
+
+
+    // Generate the default preconversion map (accents are treated as several characters so don't really matter..)
+    std::map<std::string, std::string> preconversion_map;
+    fill_conversion_map (preconversion_map, "resources/preconversion.txt");
+
+    // Generate the conversion map
+    std::map<std::string, std::string> conversion_map;
+    fill_conversion_map (conversion_map, "resources/conversion.txt");
+
+
 
     const std::string outFilePath = "output/output.wav";
 
