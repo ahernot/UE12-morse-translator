b7b7e10090f8030cdea598097770a8f7ce3b94b3
ahernot <70817470+ahernot@users.noreply.github.com>
2020-12-23 21:34:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 
+#include <string>
+#include <map>
+#include <queue>
+#include <vector>
+
 // #include "stdlib.h"
 
 #include "config.h"
 
+#include "data_loader.h"
 #include "morse.h"
 #include "signal.h"
 #include "pcm.h"
 #include "wav.h"
-
-
-
 
 
 
@@ -20,16 +23,18 @@ void textToMorseAudio (std::string& text, // cannot be const because of iterator
                        std::map<std::string, std::string> conversionMap,
                        std::map<std::string, std::string> preconversionMap) {
     
-
     // Convert from text to morse sequence
-    std::queue<std::string> conversionQueue = stringToMorse(text, conversionMap, preconversionMap);
+    std::queue<std::string> conversionQueue;
+    stringToMorse(text, conversionQueue, conversionMap, preconversionMap);
     
     // Convert from morse sequence to signal bytestream queue
+    // deletes conversionQueue (will result empty)
     std::queue<bool> signalQueue;
-    queueToSignal(conversionQueue, signalQueue); // check that conversion_queue remains unmodified (ideally would be popped and deleted)
+    queueToSignal(conversionQueue, signalQueue);
 
     // Generate PCM signal to include into WAV file
-    std::vector<int> signalPCM = generateSignalPCM (signalQueue, 44100, 1);
+    std::vector<uint8_t> signalPCM;
+    fillFromQueue (signalPCM, signalQueue, FREQUENCY, SAMPLE_RATE);
     
 
     };
@@ -37,22 +42,21 @@ void textToMorseAudio (std::string& text, // cannot be const because of iterator
 
 
 int main () {
-
-
+    
     // Generate the default preconversion map (accents are treated as several characters so don't really matter..)
-    std::map<std::string, std::string> preconversion_map;
-    fill_conversion_map (preconversion_map, "resources/preconversion.txt");
+    std::map<std::string, std::string> preconversionMap;
+    fillConversionMap (preconversionMap, "resources/preconversion.txt");
 
     // Generate the conversion map
-    std::map<std::string, std::string> conversion_map;
-    fill_conversion_map (conversion_map, "resources/conversion.txt");
+    std::map<std::string, std::string> conversionMap;
+    fillConversionMap (conversionMap, "resources/conversion.txt");
 
-
-
+    // Convert text to morse audio file
+    std::string message = "This is a message. Hello.";
     const std::string outFilePath = "output/output.wav";
+    textToMorseAudio(message, outFilePath, conversionMap, preconversionMap);
 
-
-
+    
 
     return 0;
 };
