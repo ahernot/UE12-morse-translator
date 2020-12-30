7dbe3d8f5390d02083901ccff02e2ca489e5c899
ahernot <70817470+ahernot@users.noreply.github.com>
2020-12-30 18:34:49 +0100

    add calcPseudoFrequencySquare

---
+++ src/signal_processing.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+/**
+ * Calculate pseudo-frequency of square signal (binary HIGH/LOW signal)
+ * 
+ * 
+ */
+float calcPseudoFrequencySquare (uint8_t dataUnit [],
+                                 const int& unitSize,
+                                 const int& sampleRate,
+                                 const int& low,
+                                 const int& high) {
+
+    // Initialise variables
+    float averageGap = 0;
+    int averageId = 1;
+    int tempGap = 0;
+
+    for (int i = 0; i < unitSize; i ++) {
+
+        // Read data
+        uint8_t data = dataUnit[i];
+        int dataInt = (int) data;
+
+        // 
+        if (dataInt == high) {
+            averageGap = (averageId * averageGap + tempGap) / (averageId + 1); // add to average
+            averageId ++; // increment nb of elements averaged
+            tempGap = 0; // reset gap
+        }
+        else {
+            tempGap ++;
+        };
+    };
+
+    float pseudoFrequency = sampleRate / averageGap;
+
+    // Prevent infinite value
+    if (isinf(pseudoFrequency)) {
+        return 0;
+    };
+
+    return pseudoFrequency;
+
+};
---
+++ src/signal_processing.h
@@ -0,0 +1,6 @@
+
+float calcPseudoFrequencySquare (uint8_t dataUnit [],
+                                 const int& unitSize,
+                                 const int& sampleRate,
+                                 const int& low,
+                                 const int& high);
