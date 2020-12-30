bf77b665166b481f3e5548c48ec02f6bed7b09c0
ahernot <70817470+ahernot@users.noreply.github.com>
2020-12-30 18:32:00 +0100

    Update wav.h

--- include/wav.h
+++ include/wav.h
@@ -4,17 +4,17 @@
 #include "config.h"
 
 float calcPseudoFrequencySquare (uint8_t dataUnit [],
-                                 const int unitSize,
-                                 const int sampleRate,
-                                 const int low,
-                                 const int high);
+                                 const int& unitSize,
+                                 const int& sampleRate,
+                                 const int& low,
+                                 const int& high);
 
 void generateWAV (std::vector<uint8_t>& signalPCM,
                   const std::string& outFilePath);
 
 void readWAV (const std::string& inFilePath,
               std::vector<bool>& signalVector,
-              const int frequency = FREQUENCY,
-              const int sampleRate = SAMPLE_RATE,
-              const int timeUnit = TIME_UNIT,
-              const int headerSizeBytes = 44);
+              const int& frequency = FREQUENCY,
+              const int& sampleRate = SAMPLE_RATE,
+              const int& timeUnit = TIME_UNIT,
+              const int& headerSizeBytes = 44);
